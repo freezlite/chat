769cd1723172b395b4a6a5cb58a2e8c002e63f06
magiq <matrix44@ya.ru>
2015-06-04 09:33:07 +0000

    Timestamp fixed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var moment = require('moment');
@@ -17,7 +18,7 @@ var announcement = '<u>Нововеденья: теперь работают с�
 var actions = {
   '\\/nickname (\\w{2,12})': function(nick, socket){
     if(!elementExists(nicknames_list, nick[1]) && nick[1] != 'system'){
-      old_nickname =  nicknames[socket.id];
+      var old_nickname =  nicknames[socket.id];
       nicknames[socket.id] = nick[1];
       nicknames_list.push(nick[1]);
       deleteElementIfExists(nicknames_list, old_nickname);
@@ -32,29 +33,32 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.use('/static', express.static('static'));
+
 io.on('connection', function(socket){
   user_count++;
   console.log('User connected, total users: ' + user_count)
   nicknames[socket.id] = 'guest' + Math.floor((Math.random() * 100) + 1);
-  io.emit('chat message', nicknames[socket.id] + ' joins our chat');
-  io.to(socket.id).emit('chat message', log);
-  io.to(socket.id).emit('chat message', announcement);
+  io.emit('join', nicknames[socket.id]);
+  io.to(socket.id).emit('message', log);
+  io.to(socket.id).emit('announcement', announcement);
   
   io.emit('change users count', user_count);
-  socket.on('chat message', function(msg){
+  socket.on('message', function(msg){
     if(msg){
       if(msg.length > 1000){
         io.to(socket.id).emit('error', 'Too large text');
       }else{
         var isAction = false;
         
-        for (action in actions) {
+        for (var action in actions) {
             if (!actions.hasOwnProperty(action)) {
                 //The current property is not a direct property of p
                 continue;
             }
             
-            if(match = msg.match(new RegExp(action))){
+            var match = msg.match(new RegExp(action));
+            if(match){
               isAction = true;
               actions[action](match, socket);
             }
@@ -69,11 +73,14 @@ io.on('connection', function(socket){
             }
           });
           msg = msg.replace(/(?:\r\n|\r|\n)/g, '<br />');
-          formated_msg = moment().format('HH:mm:ss') + ' ' + nicknames[socket.id] +': '+ msg;
-          log.push(formated_msg);
+          var data = {
+            'nickname': nicknames[socket.id],
+            'msg': msg,
+            'timestamp': Date.now(),
+          };
+          log.push(data);
           log = log.slice(log_count*-1);
-          console.log(formated_msg);
-          io.emit('chat message', formated_msg);
+          io.emit('message', data);
         }
       }
     }
@@ -82,7 +89,7 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     user_count--;
     console.log('User disconnected, total users: ' + user_count)
-    io.emit('chat message', nicknames[socket.id] + ' left chat.');
+    io.emit('left', nicknames[socket.id]);
     deleteElementIfExists(nicknames_list, nicknames[socket.id]);
     delete nicknames[socket.id];
     io.emit('change users count', user_count);
@@ -99,7 +106,7 @@ function elementExists(arr, element){
 }
 
 function deleteElementIfExists(arr, element){
-  index = arr.indexOf(element);
+  var index = arr.indexOf(element);
   
   if (index > -1){
     arr.splice(index, 1);
---
+++ static/main.js
@@ -0,0 +1,40 @@
+var socket = io();
+$('form').submit(function() {
+    socket.emit('message', $('#m').val());
+    $('#m').val('');
+    return false;
+});
+socket.on('message', function(data) {
+    if (data instanceof Array) {
+        for (var i in data) {
+            $('#messages').append($('<li>').html(stringFromData(data[i])));
+        }
+    } else {
+        $('#messages').append($('<li>').html(stringFromData(data)));
+    }
+    window.scrollTo(0, document.body.scrollHeight + 42);
+});
+socket.on('announcement', function(count) {
+    $('#messages').append($('<li>').html(count));
+});
+socket.on('left', function(nickname){
+    var msg = nickname + ' has left the chat';
+    $('#messages').append($('<li>').html(msg));
+});
+socket.on('join', function(nickname){
+    var msg = nickname + ' has join the chat';
+    $('#messages').append($('<li>').html(msg));
+});
+socket.on('change users count', function(count) {
+    $('#users').text('Users count: ' + count);
+});
+socket.on('error', function(msg) {
+    alert(msg);
+});
+
+function stringFromData(data){
+    var local_time = new Date(data['timestamp']);
+    var time_string = local_time.getHours() + ':' + local_time.getMinutes() + ':' + local_time.getSeconds();
+    var msg = time_string + ' ' + data['nickname'] +': '+ data['msg'];
+    return msg;
+}
