f2050ed9f063c4c3db95b38f9ea93ef8dcf9184a
Magiq <magiqm@gmail.com>
2016-05-23 11:48:27 +0300

    Jade template Class to files Captcha

--- index.js
+++ index.js
@@ -1,123 +1,5 @@
 "use strict";
 
-class User{
-    constructor(io, socket_id){
-        this.io = io;
-        this.nickname = User.generateNickName();
-        this.socket_id = socket_id;
-    }
-    
-    emit(event, message){
-        this.io.to(this.socket_id).emit(event, message);
-    }
-    
-    sendMessage(message){
-        this.emit('message', message);
-    }
-    
-    sendError(text){
-        this.emit('error', text);
-    }
-    
-    sendAnnouncment(text){
-        this.emit('announcement', text);
-    }
-    
-    sendUserList(nicknames){
-        this.emit('users', nicknames);
-    }
-    
-    static generateNickName(){
-        return 'guest' + Math.floor((Math.random() * 100) + 1)
-    }
-}
-
-class Chat{
-    constructor(io, announcement, history_limit){
-        this.io = io;
-        this.users = [];
-        this.announcement = announcement;
-        this.history = [];
-        this.history_limit=history_limit;
-    }
-    
-    addUser(user){
-        this.users.push(user);
-        io.emit('join', user.nickname);
-        io.emit('change users count', this.users.length);
-        user.sendMessage(this.history);
-        user.sendUserList(this.getNicknamesListFromUsers());
-        user.sendAnnouncment(this.announcement);
-    }
-    
-    deleteUser(user){
-        var index = this.users.indexOf(user);
-  
-        if (index > -1){
-            this.users.splice(index, 1);
-        }
-        
-        this.io.emit('change users count', this.users.length);
-        this.io.emit('left', user.nickname);
-    }
-    
-    sendMessage(message){
-        this.io.emit('message', message.toArray());
-        this.addMessageToHistory(message);
-    }
-    
-    addMessageToHistory(message){
-        this.history.push(message);
-        this.history = this.history.slice(this.history_limit*-1);
-    }
-    
-    getNicknamesListFromUsers(){
-        var nicknames = [];
-        
-        for(var i=0; i<this.users.length;i++){
-            nicknames.push(this.users[i].nickname)
-        }
-        
-        return nicknames;
-    }
-}
-
-class Message{
-    constructor(user, text, filters){
-        this.user = user;
-        this.text = text;
-        this.timestamp = Date.now();
-        this.filters = filters;
-    }
-    
-    formatText(){
-        var formattedText = this.text;
-        for(var i=0; i < this.filters.length; i++){
-            // If arguments needed
-            if (this.filters[i] instanceof Array){
-                var func = this.filters[i][0]
-                var params = this.filters[i]
-                params.splice(0, 1, formattedText)
-            } else {
-                var func = this.filters[i]
-                var params = [formattedText]
-            }
-            
-            formattedText = func.apply(this, params)
-        }
-        
-        return formattedText;
-    }
-    
-    toArray(){
-        return {
-            'nickname': this.user.nickname,
-            'msg': this.formatText(),
-            'timestamp': this.timestamp,
-        }
-    }
-}
-
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -125,12 +7,50 @@ var io = require('socket.io')(http);
 var moment = require('moment');
 var Autolinker = require('autolinker');
 var sanitizeHtml = require('sanitize-html');
+var recaptcha = require('express-recaptcha');
+var bodyParser = require('body-parser');
+var session = require('express-session')({
+  proxy: true,
+  secret: 'zaeT4Oag',
+  resave: true,
+  saveUninitialized: true,
+  secure: false
+});
+var sharedsession = require("express-socket.io-session");
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+require('./lib/chat.js')
+require('./lib/message.js')
+require('./lib/user.js')
+ 
+var port = process.env.PORT || 8080
+ 
+recaptcha.init('6Le3HCATAAAAAEXZ2UwoiOZ8_dNbssXFEP1P8ZOA', '6Le3HCATAAAAAONcgtEm9v2nPdXLJzoEIUkdO_wA');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session)
+app.use('/static', express.static('static'));
+app.enable('trust proxy')
+
+app.get('/', recaptcha.middleware.render, function(req, res){
+  if (req.session.logined) {
+    res.render('index');
+  } else {
+    res.render('login', { captcha:req.recaptcha });
+  }
+  req.session.save()
 });
 
-app.use('/static', express.static('static'));
+app.post('/', function(req, res){
+    recaptcha.verify(req, function(error){
+        if (!error) {
+            req.session.logined = true
+            req.session.save()
+            res.redirect('/')
+        }
+    })
+})
 
 var announcement = 'Welcome to chat';
 var chat = new Chat(io, announcement, 15);
@@ -147,6 +67,8 @@ var filters = [
     ],
 ];
 
+io.use(sharedsession(session));
+
 io.on('connection', function(socket){
     var user = new User(io, socket.id);
     chat.addUser(user);
@@ -161,6 +83,6 @@ io.on('connection', function(socket){
     })
 });
 
-http.listen(process.env.PORT, function(){
-  console.log('listening on *:' +  process.env.PORT);
+http.listen(port, function(){
+  console.log('listening on *:' +  port);
 });
