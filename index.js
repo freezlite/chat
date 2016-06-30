168081eef9a911709ea26b8db7dd1b0d34b1de0f
Magiq <magiqm@gmail.com>
2016-06-30 14:16:47 +0300

    Nicknames

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ var session = require('express-session')({
   secure: false
 });
 var sharedsession = require("express-socket.io-session");
+var flash = require('express-flash')
+var cookieParser = require('cookie-parser')
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use('/static', express.static('static'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser('NohJ7sae'));
+app.use(session)
+app.use(flash());
+app.enable('trust proxy')
 
 require('./lib/chat.js')
 require('./lib/message.js')
@@ -26,13 +37,6 @@ var port = process.env.PORT || 8080
  
 recaptcha.init('6Le3HCATAAAAAEXZ2UwoiOZ8_dNbssXFEP1P8ZOA', '6Le3HCATAAAAAONcgtEm9v2nPdXLJzoEIUkdO_wA');
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session)
-app.use('/static', express.static('static'));
-app.enable('trust proxy')
-
 app.get('/', recaptcha.middleware.render, function(req, res){
   if (req.session.logined) {
     res.render('index');
@@ -41,16 +45,6 @@ app.get('/', recaptcha.middleware.render, function(req, res){
   }
   req.session.save()
 });
-
-app.post('/', function(req, res){
-    recaptcha.verify(req, function(error){
-        if (!error) {
-            req.session.logined = true
-            req.session.save()
-            res.redirect('/')
-        }
-    })
-})
 
 var announcement = 'Welcome to chat';
 var chat = new Chat(io, announcement, 15);
@@ -69,8 +63,38 @@ var filters = [
 
 io.use(sharedsession(session));
 
+app.post('/', function(req, res){
+    recaptcha.verify(req, function(error){
+        if (!error) {
+            if (req.body.nickname) {
+                if (chat.isNicknameFree(req.body.nickname)) req.session.nickname = req.body.nickname
+                else {
+                    req.flash('warning', 'Nickname is not avaliable')
+                    res.redirect('/')
+                    return
+                }
+            } else {
+                req.flash('warning', 'Empty nickname')
+                res.redirect('/')
+                return
+            }
+            req.session.logined = true
+            req.session.save()
+            res.redirect('/')
+        } else {
+            req.flash('warning', 'Wrong captcha')
+            res.redirect('/')
+        }
+    })
+})
+
 io.on('connection', function(socket){
-    var user = new User(io, socket.id);
+    if (!socket.handshake.session.nickname || !socket.handshake.session.logined) {
+        chat.sendError('You not loggined');
+        socket.disconnect();
+    }
+    
+    var user = new User(io, socket.id, socket.handshake.session.nickname);
     chat.addUser(user);
     
     socket.on('message', function(text){
